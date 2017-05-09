ae7010683d8a8690b0aa39d5bb3546a0035ee22a
Delmer Izaguirre <delmerespinal2@gmail.com>
2017-05-09 14:48:30 -0600

    hola

---
+++ lab4_progra2/src/lab4_progra2/Antirrobo.java
@@ -0,0 +1,14 @@
+package lab4_progra2;
+
+import java.util.Random;
+
+public abstract class Antirrobo {
+
+    public Antirrobo() {
+    }
+public abstract String riel();
+public abstract String riel();
+public abstract String riel();
+    
+
+}
